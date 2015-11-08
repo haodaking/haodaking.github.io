a8e4b456d551de7d7c6df23d3ad44768c1f9cec0
haodaking <haodaking@163.com>
2015-11-08 11:24:06 +0800

    update

--- js/base.js
+++ js/base.js
@@ -1,34 +1,36 @@
-;(function () {
-    var openMenu = document.querySelector('#menuToggle');
-    var closeMenu = document.querySelector('.close-menu');
-    var oMenu = document.querySelector('.menu');
-    var menuOverlord = document.querySelector('#menuOverlord')
-    var pageoverlay = document.querySelector('#page-overlay');
-    var bFlag = false;
-    openMenu.addEventListener('click', openMenuFn, false);
-    closeMenu.addEventListener('click', closeMenuFn, false);
-    pageoverlay.addEventListener('click', closeMenuFn, false);
-    
-    function openMenuFn() {
-        if (bFlag) {
-            closeMenuFn();
-            return;
-        }
-        bFlag = true;
-        oMenu.classList.add('menu-open');
-        oMenu.classList.remove('menu-close');
-        closeMenu.style.display = 'block';
-        menuOverlord.classList.remove('closeOverlord');
-        menuOverlord.classList.add('openOverlord');
-        pageoverlay.style.display = 'block';
+function ready(fn) {
+    if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', fn, false)
     }
-    function closeMenuFn() {
-        bFlag = false;
-        oMenu.classList.remove('menu-open');
-        oMenu.classList.add('menu-close');
-        closeMenu.style.display = 'none';
-        menuOverlord.classList.remove('openOverlord');
-        menuOverlord.classList.add('closeOverlord');
-        pageoverlay.style.display = 'none';
+    else {
+        document.attachEvent('onreadystatechange',
+            function () {
+                if (document.readyState == 'complete') {
+                    fn();
+                }
+            }
+        )
     }
-})()
+}
+ready(function () {
+    ;(function () {
+        var oMenuToggle = document.querySelector('#js-menuToggle');
+        var oMenu = document.querySelector('#js-menu');
+        var oPageInner = document.querySelector('#js-pageInner');
+        var bFlag = false;
+        oMenuToggle.addEventListener('click', function () {
+            if (!bFlag) {
+                bFlag = true;
+                oMenuToggle.classList.add('active');
+                oMenu.classList.add('offset');
+                oPageInner.classList.add('offset');
+            }
+            else {
+                bFlag = false;
+                oMenuToggle.classList.remove('active');
+                oMenu.classList.remove('offset');
+                oPageInner.classList.remove('offset');
+            }
+        }, false)
+    })();
+})
